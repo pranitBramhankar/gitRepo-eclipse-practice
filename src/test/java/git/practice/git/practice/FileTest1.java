7d1dd210014746615f2d2e8bbbc90ce468889705
pranit <pranitsb1997@gmail.com>
2026-04-11 00:55:20 +0530

    Adding FileTest1 from tester system

---
+++ src/test/java/git/practice/git/practice/FileTest1.java
@@ -0,0 +1,12 @@
+package git.practice.git.practice;
+
+public class FileTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ading file from sinior tester");
+
+	}
+
+}
