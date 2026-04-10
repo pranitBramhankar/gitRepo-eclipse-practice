fab031618106ab9dbca92b280373f5e73969ad36
Yogesh(Pranit) <pranitsb1997@gmail.com>
2026-04-10 12:48:33 +0530

    Adding File 1 to local Repository

---
+++ src/test/java/git/practice/git/practice/File1.java
@@ -0,0 +1,12 @@
+package git.practice.git.practice;
+
+public class File1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Pranit");
+
+	}
+
+}
