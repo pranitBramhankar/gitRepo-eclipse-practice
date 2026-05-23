e19153714f75f95df7a638387b39efd0693c4893
pranitBramhankar <pranitsb1997@gmail.com>
2026-05-23 08:04:56 +0530

    Adding changes in File1

--- src/test/java/git/practice/git/practice/File1.java
+++ src/test/java/git/practice/git/practice/File1.java
@@ -6,8 +6,11 @@ public class File1 {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Pranit");
+		System.out.println("Adding this line to create a conflict");
 		System.out.println("Pranit1");
+		System.out.println("Adding second line to create a conflict");
 		System.out.println("Pranit2");
+		System.out.println("Adding third line to create a conflict");
 		System.out.println("Adding to Feature1");
 	
 
